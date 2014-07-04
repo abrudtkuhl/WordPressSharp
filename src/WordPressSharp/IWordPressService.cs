143b3946e926eea8b9d9ae5df71559603881a45e
Andy Brudtkuhl <abrudtkuhl@gmail.com>
2014-07-03 23:05:13 -0500

    wp.getPosts support

--- src/WordPressSharp/IWordPressService.cs
+++ src/WordPressSharp/IWordPressService.cs
@@ -8,6 +8,9 @@ namespace WordPressSharp
         [XmlRpcMethod("wp.getPost")]
         Post GetPost(int blog_id, string username, string password, int post_id);
 
+        [XmlRpcMethod("wp.getPosts")]
+        Post[] GetPosts(int blog_id, string username, string password, PostFilter filter);
+
         [XmlRpcMethod("wp.newPost")]
         string NewPost(int blog_id, string username, string password, Post post);
     }
