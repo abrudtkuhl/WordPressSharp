9980cbd702771b4013ef55c7f878b95b8d2843ca
Andy Brudtkuhl <abrudtkuhl@pork.org>
2014-07-07 11:25:47 -0500

    wp.getPosts integration

--- src/WordPressSharp/IWordPressService.cs
+++ src/WordPressSharp/IWordPressService.cs
@@ -5,8 +5,12 @@ namespace WordPressSharp
 {
     public interface IWordPressService : IXmlRpcProxy
     {
+        // POSTS
         [XmlRpcMethod("wp.getPost")]
         Post GetPost(int blog_id, string username, string password, int post_id);
+
+        [XmlRpcMethod("wp.getPosts")]
+        Post[] GetPosts(int blog_id, string username, string passwrod, PostFilter postFilter);
 
         [XmlRpcMethod("wp.newPost")]
         string NewPost(int blog_id, string username, string password, Post post);
--- src/WordPressSharp/WordPressClient.cs
+++ src/WordPressSharp/WordPressClient.cs
@@ -8,8 +8,15 @@ namespace WordPressSharp
     {
         protected WordPressSiteConfig WordPressSiteConfig { get; set; }
 
+        /// <summary>
+        /// The interface extension for the CookComputing.XmlRpc proxy
+        /// </summary>
         public IWordPressService WordPressService { get; internal set; }
 
+        /// <summary>
+        /// Initialize a new instance of the WordPress Client class
+        /// </summary>
+        /// <param name="siteConfig">WordPress Site Config</param>
         public WordPressClient(WordPressSiteConfig siteConfig)
         {
             WordPressSiteConfig = siteConfig;
@@ -18,11 +25,32 @@ namespace WordPressSharp
             WordPressService.Url = WordPressSiteConfig.FullUrl;
         }
 
+        /// <summary>
+        /// Gets a WordPress post by post_id
+        /// </summary>
+        /// <param name="postId">The Post Id</param>
+        /// <returns>Post</returns>
         public Post GetPost(int postId)
         {
             return WordPressService.GetPost(WordPressSiteConfig.BlogId, WordPressSiteConfig.Username, WordPressSiteConfig.Password, postId);
         }
 
+        /// <summary>
+        /// Gets a list of posts
+        /// </summary>
+        /// <param name="postFilter">A Post Filter</param>
+        /// <returns></returns>
+        public Post[] GetPosts(PostFilter postFilter)
+        {
+            return WordPressService.GetPosts(WordPressSiteConfig.BlogId, WordPressSiteConfig.Username,
+                WordPressSiteConfig.Password, postFilter);
+        }
+
+        /// <summary>
+        /// Creates a new entry in WordPress determined by Post.PostType to be a "Post", "Page", or custom post type
+        /// </summary>
+        /// <param name="post">The post to create</param>
+        /// <returns></returns>
         public string NewPost(Post post)
         {
             return WordPressService.NewPost(WordPressSiteConfig.BlogId, WordPressSiteConfig.Username, WordPressSiteConfig.Password, post);
