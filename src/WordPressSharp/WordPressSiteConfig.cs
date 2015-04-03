ae794bbe6db14653a837ef6b3f66c1640cb6deee
KeesCBakker <c.bakker@gmx.net>
2015-04-03 22:28:47 +0200

    Created WordPressSiteConfig.Default that uses the configuration to get the values. It is used by the default constructor of the WordPressClient.

--- src/WordPressSharp/WordPressSiteConfig.cs
+++ src/WordPressSharp/WordPressSiteConfig.cs
@@ -1,21 +1,75 @@
 ﻿using System.ComponentModel;
 using CookComputing.XmlRpc;
+using System;
+using System.Configuration;
 
 namespace WordPressSharp
 {
-    [XmlRpcMissingMapping(MappingAction.Ignore)]
-    public class WordPressSiteConfig
-    {
-        [XmlRpcMember("blog_id")]
-        public int BlogId { get; set; }
+	/// <summary>
+	/// Stores the configuration for the WordPressClient. Use Default to construct a configuration based on the following app settings: 
+	/// WordPressUserName, WordPressPassword, WordPressBaseUrl and WordPressBlogId (optional).
+	/// </summary>
+	[XmlRpcMissingMapping(MappingAction.Ignore)]
+	public class WordPressSiteConfig
+	{
+		/// <summary>
+		/// Gets the default configuration. This configuration looks for the following app settings: 
+		/// WordPressUserName, WordPressPassword, WordPressBaseUrl and WordPressBlogId (optional).
+		/// </summary>
+		/// <value>
+		/// The default.
+		/// </value>
+		public static WordPressSiteConfig Default { get { return _defaultConfig.Value; } }
 
-        [XmlRpcMember("username")]
-        public string Username { get; set; }
+		[XmlRpcMember("blog_id")]
+		public int BlogId { get; set; }
 
-        [XmlRpcMember("password")]
-        public string Password { get; set; }
+		[XmlRpcMember("username")]
+		public string Username { get; set; }
 
-        public string BaseUrl { get; set; }
-        public string FullUrl { get { return string.Concat(BaseUrl, BaseUrl.EndsWith("/") ? "xmlrpc.php" : "/xmlrpc.php"); } }
-    }
+		[XmlRpcMember("password")]
+		public string Password { get; set; }
+
+		public string BaseUrl { get; set; }
+
+		public string FullUrl { get { return string.Concat(BaseUrl, BaseUrl.EndsWith("/") ? "xmlrpc.php" : "/xmlrpc.php"); } }
+
+		#region Create default configuration from app settings (WordPressUsername, WordPressPassword, WordPressBaseUrl, WordPressBlogId
+
+		/// <summary>
+		/// Creates the default configuration.
+		/// </summary>
+		private static readonly Lazy<WordPressSiteConfig> _defaultConfig = new Lazy<WordPressSiteConfig>(() =>
+		{
+			var config = new WordPressSiteConfig
+			{
+				Username = ConfigurationManager.AppSettings["WordPressUsername"],
+				Password = ConfigurationManager.AppSettings["WordPressPassword"],
+				BaseUrl = ConfigurationManager.AppSettings["WordPressBaseUrl"]
+			};
+
+			int blogId = 0;
+			if (Int32.TryParse(ConfigurationManager.AppSettings["WordPressBlogId"], out blogId))
+			{
+				config.BlogId = blogId;
+			}
+
+			if (String.IsNullOrEmpty(config.Username))
+			{
+				throw new ConfigurationException("Missing WordPressUsername. Please add it to your .config file.");
+			}
+			if (String.IsNullOrEmpty(config.Password))
+			{
+				throw new ConfigurationException("Missing WordPressPassword. Please add it to your .config file.");
+			}
+			if (String.IsNullOrEmpty(config.BaseUrl))
+			{
+				throw new ConfigurationException("Missing WordPressBaseUrl. Please add it to your .config file.");
+			}
+
+			return config;
+		});
+
+		#endregion
+	}
 }
